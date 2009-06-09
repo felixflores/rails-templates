200b41c4a1b52e62d18be270d7cbbcd98abea8fc
Felix Flores <felixflores@gmail.com>
2009-06-08 20:41:10 -0400

    Fix destroy action on UserSessions

--- authenticated_user_session.rb
+++ authenticated_user_session.rb
@@ -47,7 +47,7 @@ class UserSessionsController < ApplicationController
   def destroy
     current_user_session.destroy
     flash[:notice] = "Logout successful!"
-    redirect_back_or_default root_url
+    redirect_to root_url
   end
 end
 CODE
